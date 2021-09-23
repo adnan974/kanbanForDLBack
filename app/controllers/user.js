4522dc6979721383576f5066729aa7659f718244
Adnan <adnan.goulamaly@gmail.com>
2021-09-23 15:39:17 +0400

    feature: add new route to get all users

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,4 +1,18 @@
+const User = require('../models/User')
 // TODO
+
+/**
+ * This function is only for dev purpose
+ * @route GET /users
+ * @group Users - Operations about user
+ * @returns {object} 200 - An array of tickets info
+ * @returns {Error}  default - Unexpected error
+ */
+exports.getAllusers = (req,res)=>{
+    User.find().then(users=>{
+        return res.status(200).json(users);
+    })
+}
 exports.deleteUser = (req,res) =>{
     return res.status(404).json("delete user");
 }
--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -3,11 +3,12 @@ const router = express.Router();
 const { body } = require('express-validator');
 
 
-const { deleteUser } = require('../../controllers/user');
+const { deleteUser, getAllusers } = require('../../controllers/user');
 const { postUserTicket } = require('../../controllers/user-ticket');
 const { getUserNotifications } = require('../../controllers/user-notification');
 
 
+router.get('/', getAllusers);
 router.delete('/:id', deleteUser);
 
 router.post(

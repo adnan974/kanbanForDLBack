04fda75dabd40ab880a159831e81814c6054397e
Adnan <adnan.goulamaly@gmail.com>
2021-09-30 15:55:30 +0400

    feature: add new route for updating a dashboard

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -39,6 +39,39 @@ exports.getDashboard = (req, res) => {
 
 
 /**
+ * This function comment is parsed by doctrine
+ * @route PATCH /dashboards/{id}
+ * @group Dashboards - Operations about dashboards
+ * @param {string} id.path.required - dashboard id
+ * @param {UpdateDashboardDTO.model} dashboard.body.required - the updated dashboard
+ * @returns {object} 200 - Succes: ticket updated
+ * @returns {Error}  default - Unexpected error
+ */
+ exports.updateDashboard = (req, res, next) => {
+
+  const dashboardId = req.params.id;
+
+  const {
+    title,
+    description,
+    labels
+  } = req.body;
+
+  Dashboard.findByIdAndUpdate(dashboardId, {
+    title,
+    description,
+    labels
+  }, (error) => {
+    if (error) {
+      res.status(422).json({ error });
+    } else {
+      res.send("dashboard updated");
+    }
+  })
+}
+
+
+/**
 * This function comment is parsed by doctrine
 * @route DELETE /dashboards/{id}
 * @group Dashboards - Operations about dashboards
--- app/routes/dashboard/index.js
+++ app/routes/dashboard/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { body } = require('express-validator');
 const router = express.Router();
 
-const { deleteDashboard, getDashboard, getAllDashboards } = require('../../controllers/dashboard');
+const { deleteDashboard, getDashboard, getAllDashboards, updateDashboard } = require('../../controllers/dashboard');
 const { addColumnToDashboard, getColumn, deleteColumn } = require('../../controllers/dashboard-column');
 
 router.get('', getAllDashboards);
 router.get('/:id', getDashboard);
 router.delete('/:id', deleteDashboard);
+router.patch('/:id', updateDashboard);
 
 router.post('/:id/columns', body('title').notEmpty(), addColumnToDashboard);
 router.get('/:id/columns', getColumn);

310fd26fcd1159758082bb3f18c3ac41fe635d4e
Adnan <adnan.goulamaly@gmail.com>
2021-10-22 13:55:51 +0200

    delete code of precedent commit

--- app/controllers/column.js
+++ app/controllers/column.js
@@ -8,7 +8,6 @@ const Column = require("../models/Column");
  * @returns {Error}  default - Unexpected error
  */
 exports.getAllColumns = (req, res) => {
-    return res.status(200).json("test");
     Column.find().then(data => {
         res.status(200).json({ data })
     }).catch(error => {
